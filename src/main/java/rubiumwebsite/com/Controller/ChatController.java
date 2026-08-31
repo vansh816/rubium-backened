b289856b9eddbde3a9b95a2977c3b079e2546885
Vansh816 <sharmavn258@email.com>
2026-08-31 14:23:07 +0530

    Secure application configuration

--- src/main/java/rubiumwebsite/com/Controller/ChatController.java
+++ src/main/java/rubiumwebsite/com/Controller/ChatController.java
@@ -6,39 +6,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-//@RestController
-//@RequestMapping("/api/chat")
-//@CrossOrigin(origins = "*")
-//public class ChatController {
-//
-//    private final ChatService chatService;
-//
-//    public ChatController(ChatService chatService) {
-//        this.chatService = chatService;
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<?> chat(
-//            @RequestBody Map<String, String> request
-//    ) {
-//
-//        String message = request.get("message");
-//
-//        if (message == null || message.isBlank()) {
-//            return ResponseEntity.badRequest()
-//                    .body(Map.of("error", "Message cannot be empty"));
-//        }
-//
-//        String reply = chatService.chatResponse(message);
-//
-//        return ResponseEntity.ok(
-//                Map.of("reply", reply)
-//        );
-//    }
-//}
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://rubiumai.com")
 public class ChatController {
 
     private final ChatService chatService;
